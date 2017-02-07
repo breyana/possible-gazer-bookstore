80be53520601f7dc3ca370e2e4ba90a1a2469913
Breyana Scales <breyana@Breyanas-MacBook-Pro.local>
2017-02-06 20:27:31 -0800

    added in a test page and changed routes to be more specific

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 const db = require('../queries');
 
+
 router.get('/', db.getAllBooks);
-router.get('/:id', db.getSingleBook);
-router.get('/books/:author', db.getSingleAuthor);
-router.get('/genres/:genre', db.getSingleGenre);
+router.get('/testpage', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/author/:author', db.getSingleAuthor);
+router.get('/genre/:genre', db.getSingleGenre);
+router.get('/book/:id', db.getSingleBook);
 
 router.post('/', db.createBook);
 
